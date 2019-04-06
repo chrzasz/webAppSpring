b6a2264781ef9c612fcc07d01cbb7dcb9d99f260
Greg <chrzaszczyk@gmail.com>
2019-04-06 11:00:15 +0200

    mapping

--- src/main/java/pl/sda/webApp/controlers/MyFirstController.java
+++ src/main/java/pl/sda/webApp/controlers/MyFirstController.java
@@ -1,13 +1,23 @@
 package pl.sda.webApp.controlers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import pl.sda.webApp.model.Animal;
 
 @RestController
 public class MyFirstController {
 
-    @GetMapping("/hello")
-    public String helloWorld() {
-        return "Hello world";
+    @GetMapping("/animal")
+    public Animal animal(
+            @RequestParam(name = "name", defaultValue = "default") String name) {
+        Animal animal = new Animal(name, 8);
+        return animal;
+    }
+
+    @GetMapping("/favourite/{number}")
+    public String favourite(@PathVariable("number") String number) {
+        return number;
     }
 }
---
+++ src/main/java/pl/sda/webApp/model/Animal.java
@@ -0,0 +1,12 @@
+package pl.sda.webApp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Animal {
+
+    private String name;
+    private int age;
+}

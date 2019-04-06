6985bee1056786cc2cd113b9dbe780a2f631406e
Greg <chrzaszczyk@gmail.com>
2019-04-06 14:09:27 +0200

    adding PUT

--- src/main/java/pl/sda/webApp/controlers/AnimalController.java
+++ src/main/java/pl/sda/webApp/controlers/AnimalController.java
@@ -4,22 +4,55 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import pl.sda.webApp.model.Animal;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/animals")
 public class AnimalController {
 
-    private List<Animal> animalStorage = new ArrayList<>();
+    private Map<UUID, Animal> animalStorage = new HashMap<>();
 
     @GetMapping(value = "/list", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public List<Animal> animals() {
+    public Map<UUID, Animal> animals() {
         return animalStorage;
     }
 
     @PostMapping("/add")
-    public void addAnimal(@RequestBody Animal animal) {
-        animalStorage.add(animal);
+    public UUID addAnimal(@RequestBody Animal animal) {
+        UUID uuid = UUID.randomUUID();
+        animalStorage.put(uuid, animal);
+        return uuid;
     }
+
+    @DeleteMapping("/remove/{id}")
+    public boolean remove(@PathVariable UUID id) {
+        if (animalStorage.containsKey(id)) {
+            animalStorage.remove(id);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @PutMapping("/edit/{id}")
+    public boolean edit(@RequestBody Animal animal, @PathVariable UUID id) {
+        if (!animalStorage.containsKey(id) || animal == null) {
+            throw new IllegalArgumentException("Wrong argument");
+        }
+        animalStorage.replace(id, animal);
+        return true;
+    }
+
+    @PutMapping("/edit-name/{id}")
+    public boolean editName(@RequestParam("name") String name, @PathVariable UUID id) {
+        if (!animalStorage.containsKey(id)) {
+            throw new IllegalArgumentException("Wrong argument");
+        }
+        animalStorage.get(id).setName(name);
+        return true;
+    }
+
+
 }
